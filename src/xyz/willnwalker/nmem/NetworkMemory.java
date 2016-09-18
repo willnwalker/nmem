9900b5bf36327a2e753809c1eef7f592c8545881
William <willnwalker@outlook.com>
2016-09-17 21:09:29 -0700

    Feature Additions
    Changes:
    More Verbose usage
    Changed order of modifiers in class variables
    Additions:
    Added date formatting, which allows differentiating log files for each
    session, and seeing when each session started
    Added screen initialization routines
    Added storage initialization routines
    Added template for ServerListener, which will responsd to network
    requests

--- src/xyz/willnwalker/nmem/NetworkMemory.java
+++ src/xyz/willnwalker/nmem/NetworkMemory.java
@@ -1,15 +1,23 @@
 package xyz.willnwalker.nmem;
+
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.jmr.wrapper.common.Connection;
+import com.jmr.wrapper.common.listener.SocketListener;
 import com.jmr.wrapper.server.Server;
-import com.jmr.wrapper.common.exceptions.NNCantStartServer;
 
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * <h1>Network Memory</h1>
  * The NetworkMemory program implements a dynamically-sized memory bank that can be
- * accessed using standard Java Networking Sockets on port 2148.
+ * accessed using standard Java Networking Sockets on TCP port 2148.
  *
  * @author William Walker
  * @version 1.0
@@ -17,15 +25,17 @@ import java.util.Date;
  */
 public class NetworkMemory {
 
-    static private Server server;
-    static private PrintWriter log;
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd.HHmmss");
+    private static Server server;
+    private static PrintWriter log;
 
     public static void main(String[] args){
 
         try{
             //Log Initialization
-            Date date = new Date();
-            log = new PrintWriter(new File("log.txt"));
+            String init_time = DATE_FORMAT.format(new Date());
+            log = new PrintWriter(new File("log"+init_time+".txt"));
+            writeOut("Log initialized successfully at "+init_time+".");
 
             //Server Initialization
             server = new Server(2148, 2148);
@@ -34,7 +44,14 @@ public class NetworkMemory {
             }
 
             //Screen Initialization
-            
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            writeOut("Terminal size: "+screen.getTerminalSize().toString());
+
+            //Storage Initialization
+            //byte[] bank = new byte[100000000];
+            byte[] bank = new byte[2000];
+
         }
         catch(Exception e){
             e.printStackTrace();
@@ -47,6 +64,25 @@ public class NetworkMemory {
         System.out.println(s);
         log.println(s);
         log.flush();
+    }
+
+    private class ServerListener implements SocketListener{
+
+        @Override
+        public void received(Connection con, Object obj){
+            return;
+        }
+
+        @Override
+        public void connected(Connection con){
+            return;
+        }
+
+        @Override
+        public void disconnected(Connection con){
+
+        }
+
     }
 
 }
